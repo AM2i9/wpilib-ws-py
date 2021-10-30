a16f9c064d583a44e507c086d74c16b36e1b0104
Patrick Brennan <brennanpatrick20@gmail.com>
2021-10-30 14:47:28 -0400

    Correct misnamed import

--- wpilib_ws/__init__.py
+++ wpilib_ws/__init__.py
@@ -1,2 +1,2 @@
-from wpilib_ws.server import Server, MessageEvent, InvalidDeviceError
+from wpilib_ws.server import WPILibWsServer, MessageEvent, InvalidDeviceError
 from wpilib_ws.hardware import DeviceType, CANDeviceType
